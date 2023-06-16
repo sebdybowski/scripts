c8bd43f056d419f5bfe24c3563d04947a17e75ad
Seb Dybowski <dybowski.seb@gmail.com>
2023-06-16 23:30:05 +0200

    pnpm correction

--- pnpm.sh
+++ pnpm.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export PNPM_HOME="/home/seb/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
 *":$PNPM_HOME:"*) ;;
 *) export PATH="$PNPM_HOME:$PATH" ;;
