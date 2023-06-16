3929bd7d11f0b6f46b4500e743229d7039202287
Seb Dybowski <dybowski.seb@gmail.com>
2023-06-16 23:31:33 +0200

    init mod

--- init.sh
+++ init.sh
@@ -12,12 +12,11 @@ source "${__HOME_DIR}/git.sh"
 
 # Package management
 # source "${__HOME_DIR}/pacman.sh"
-# source "${__HOME_DIR}/dnf.sh"
-source "${__HOME_DIR}/apt.sh"
+source "${__HOME_DIR}/dnf.sh"
+# source "${__HOME_DIR}/apt.sh"
 
 # Utils
 source "${__HOME_DIR}/draw.sh"
 source "${__HOME_DIR}/neovim.sh"
 source "${__HOME_DIR}/flatpak.sh"
 # source "${__HOME_DIR}/gogh.sh"
-
