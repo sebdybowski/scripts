92b95a63eddae58628a839d0f3635f60916a8a7c
Seb Dybowski <dybowski.seb@gmail.com>
2025-05-04 22:46:33 +0200

    gh & cleanup

--- init.sh
+++ init.sh
@@ -9,14 +9,15 @@ source "${__HOME_DIR}/nvm.sh"
 source "${__HOME_DIR}/pnpm.sh"
 source "${__HOME_DIR}/ls.sh"
 source "${__HOME_DIR}/git.sh"
+source "${__HOME_DIR}/gh.sh"
 
 # Package management
 # source "${__HOME_DIR}/pacman.sh"
-source "${__HOME_DIR}/dnf.sh"
+# source "${__HOME_DIR}/dnf.sh"
 # source "${__HOME_DIR}/apt.sh"
+# source "${__HOME_DIR}/flatpak.sh"
 
 # Utils
 source "${__HOME_DIR}/draw.sh"
-source "${__HOME_DIR}/neovim.sh"
-source "${__HOME_DIR}/flatpak.sh"
-# source "${__HOME_DIR}/gogh.sh"
+# source "${__HOME_DIR}/neovim.sh"
+
